3f92691a15c5b71ff109a3a26e964d316be49793
João Victor Braga <joaovab@al.insper.edu.br>
2026-02-20 14:30:14 -0300

    docs: adição de documentação a funçção download_laz_quadra

--- src/download_laz.py
+++ src/download_laz.py
@@ -4,6 +4,15 @@ import zipfile
 import io
 
 def download_laz_quadra(codigo_quadra: str, pasta_destino:str) -> None:
+    """
+    Baixa arquivo .laz do site da Prefeitura de São Paulo, GeoSampa, para a quadra especificada e extrai o conteúdo para a pasta de destino.
+
+    Args:
+        codigo_quadra (str): Código da quadra a ser baixada.
+        pasta_destino (str): Caminho da pasta onde o arquivo extraído será salvo.
+    Returns:
+        None
+    """
     url_download = f"https://download.geosampa.prefeitura.sp.gov.br/PaginasPublicas/downloadArquivo.aspx?orig=DownloadMapaArticulacao&arq=MDS_2020%5C{codigo_quadra}.zip&arqTipo=MAPA_ARTICULACAO"
 
     os.makedirs(pasta_destino, exist_ok=True)
